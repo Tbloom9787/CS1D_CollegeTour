9b67e990fcc4526dfe6f64159d8f35a5c043e7b6
Tyler Bloom <tylerbloom9787@gmail.com>
2018-09-19 00:56:47 -0700

    Implemented the model of college methods and UI

---
+++ collegemodel.cpp
@@ -0,0 +1,334 @@
+#include "collegemodel.h"
+#include "ui_collegemodel.h"
+#include "QInputDialog"
+#include <QMessageBox>
+
+CollegeModel::CollegeModel(College collegeClicked, bool startingFromSaddleback, QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::CollegeModel)
+{
+    ui->setupUi(this);
+    if (startingFromSaddleback)
+    {
+        ui->totalTripDistanceLabel->setText("Total Trip Distance: " + QString::number(collegeClicked.distanceToSaddleback, 'f', 2) + " miles");
+        totalDistance = collegeClicked.distanceToSaddleback;
+    } else {
+
+        totalDistance = 0;
+    }
+
+    // Display the name of the next closest college in the text of the UI button.
+    QVector<Distance> distances = DatabaseManager::getInstance()->getDistancesFrom(collegeClicked.id);
+    College nextClosest = DatabaseManager::getInstance()->getCollegeByID(distances[0].destinationCollege_ID);
+    ui->nextClosestCollegeButton->setText("Next Closest College\n(" + nextClosest.name + ")");
+
+    qDebug() << "College Clicked: " << collegeClicked.name;
+    // All the data affiliated with the college that was clicked.
+    this->collegeClicked = collegeClicked;
+    populateMenuItemDisplay(collegeClicked.id);
+    QListWidgetItem* newLabel = new QListWidgetItem(collegeClicked.name);
+    newLabel->setBackgroundColor(Qt::red);
+    newLabel->setFlags(newLabel->flags() & ~Qt::ItemIsSelectable);
+    ui->cartListWidget->addItem(newLabel);
+}
+
+CollegeModel::~CollegeModel()
+{
+    delete ui;
+}
+
+void CollegeModel::updateShoppingCart(MenuItem item)
+{
+    QListWidgetItem* newItem = new QListWidgetItem;
+    newItem->setData(Qt::UserRole, item.id);
+    newItem->setData(128, collegeClicked.id);
+    newItem->setText(" °  " + item.name + " - $" + QString::number(item.price));
+    qDebug() << newItem->data(Qt::UserRole).toInt();
+    ui->cartListWidget->addItem(newItem);
+}
+
+void CollegeModel::populateMenuItemDisplay(int collegeID)
+{
+    clearWidgets(ui->menuGridLayout);
+    College currentCollege = DatabaseManager::getInstance()->getCollegeByID(collegeID);
+    QVector<MenuItem> menuItems = currentCollege.menuItems;
+
+    // Set up labels
+    QString distanceLabel = QString::number(currentCollege.distanceToSaddleback);
+    ui->collegeNameLabel->setText(currentCollege.name);
+    this->setWindowTitle("Viewing College: " + currentCollege.name);
+    ui->totalTripDistanceLabel->setText("Distance to saddleback: " + distanceLabel + " miles");
+
+    qDebug() << "menu items: " << menuItems.size();
+    // Populates the menu item buttons on menuGridLayout
+    if (menuItems.size() > 0)
+    {
+        int row = 0;
+        int col = 0;
+        for (int menuItemIndex=0; menuItemIndex < menuItems.size(); menuItemIndex++)
+        {
+            if (col == 5)
+            {
+                row++;
+                col = 0;
+            }
+            QPushButton* menuItemButton = new QPushButton(menuItems[menuItemIndex].name + "\n$ " + QString::number(menuItems[menuItemIndex].price));
+            menuItemButton->setMaximumHeight(200);
+
+            menuItemButton->setObjectName(QString::number(menuItems[menuItemIndex].id));
+
+            connect(menuItemButton, SIGNAL(clicked()), this, SLOT(menuItemButtonPressed()));
+
+            menuItemButton->setSizePolicy(QSizePolicy::MinimumExpanding,QSizePolicy::MinimumExpanding);
+            ui->menuGridLayout->addWidget(menuItemButton, row, col);
+            col++;
+        }
+    }
+    this->collegeClicked = currentCollege;
+}
+
+void CollegeModel::menuItemButtonPressed()
+{
+    // Get the QPushButton object that was clicked
+    QPushButton *clickedMenuItemButton = qobject_cast<QPushButton*>(sender());
+    // get the ID of menuItem clicked
+    int clickedMenuItemID = clickedMenuItemButton->objectName().toInt();
+
+    // Get menu item data from database using the ID
+    MenuItem clickedMenuItem = DatabaseManager::getInstance()->getMenuItemByID(clickedMenuItemID);
+
+    // Prompt the user to confirm their purchase
+    confirmPurchase(clickedMenuItem);
+}
+
+void CollegeModel::confirmPurchase(MenuItem item)
+{
+    QMessageBox::StandardButton confirm;
+    confirm = QMessageBox::question(this, "Confirm Purchase", "Are you sure you want to purchase this item?",
+                                    QMessageBox::Yes|QMessageBox::No);
+    if (confirm == QMessageBox::Yes) {
+        // Add the menu item to the shopping cart, and update the UI!
+        shoppingCart.addTransaction(collegeClicked, item);
+        updateShoppingCart(item);
+    } else {
+        // Do nothing
+    }
+
+}
+void CollegeModel::clearWidgets(QLayout * layout) {
+    if (! layout)
+        return;
+    while (auto item = layout->takeAt(0)) {
+        delete item->widget();
+        clearWidgets(item->layout());
+    }
+}
+
+void CollegeModel::getTripLengthFromUser()
+{
+    bool ok;
+    // get user input as soon as the CollegeModel is instantiated from mainmenu.cpp button click event
+    int numOfCollegesInput = QInputDialog::getInt(this, tr("Number of College Campus Stops"),
+                                                     tr("Please enter the total number of colleges you wish to visit:"), 1, 0, DatabaseManager::getInstance()->getNumberOfColleges(), 1, &ok);
+    if (ok) // save the user's input for later use
+    {
+        this->numberOfCollegesToVisit = numOfCollegesInput;
+        if (this->numberOfCollegesToVisit == 1)
+        {
+            ui->nextClosestCollegeButton->setText("End Trip (Checkout)");
+        }
+    } else {
+        // The user did not specify a number of colleges to visit, so close the restuarant view window.
+        this->close();
+    }
+}
+
+void CollegeModel::recursivePathPlanner(College currentCollege, QVector<College>& mostEffecientList)
+{
+    qDebug() << currentCollege.name;
+    mostEffecientList.push_back(currentCollege);
+    // cut off the algorithm once we have calculated only the number of restuarants the user wants to visit, nothing more
+    if (mostEffecientList.size() == this->numberOfCollegesToVisit)
+    {
+        return;
+    } else {
+        QVector<Distance> distances = DatabaseManager::getInstance()->getDistancesFrom(currentCollege.id);
+
+        College nextClosest = DatabaseManager::getInstance()->getCollegeByID(distances.at(0).destinationCollege_ID);
+        qDebug() << "Next closest: " << nextClosest.name;
+        int index = 1;
+        if (vectorContains(mostEffecientList, nextClosest))
+        {
+            while(vectorContains(mostEffecientList, nextClosest))
+            {
+                nextClosest = DatabaseManager::getInstance()->getCollegeByID(distances.at(index).destinationCollege_ID);
+                index++;
+            }
+            totalDistance += distances.at(index-1).distanceTo;
+            qDebug() << "Adding distance from " << currentCollege.name << " to: " << DatabaseManager::getInstance()->getCollegeByID(distances.at(index).destinationCollege_ID).name;
+        } else {
+            totalDistance += distances.at(0).distanceTo;
+            qDebug() << "Adding distance 0 from " << currentCollege.name << " to: " << DatabaseManager::getInstance()->getCollegeByID(distances.at(0).destinationCollege_ID).name;
+        }
+        totalDistancesIndexed.push_back(totalDistance);
+
+        recursivePathPlanner(nextClosest, mostEffecientList);
+    }
+}
+
+void CollegeModel::on_nextClosestCollegeButton_clicked()
+{
+    if (numberOfCollegesToVisit == 1)
+    {
+        this->close();
+    } else if (numberOfCollegesToVisit != 1)
+    {
+        if (mostEffecientTrip.size() == 0)
+        {
+            QVector<Distance> distances = DatabaseManager::getInstance()->getDistancesFrom(collegeClicked.id);
+            College nextClosest = DatabaseManager::getInstance()->getCollegeByID(distances.at(0).destinationCollege_ID);
+
+            qDebug() << "Distances: " << distances.size();
+            if (numberOfCollegesToVisit == 2)
+            {
+                // Handles the case when the user only wants to visit the initial college and one college after!
+                populateMenuItemDisplay(distances.at(0).destinationCollege_ID);
+                qDebug() << "Total distance: " << totalDistance;
+                qDebug() << "Distance between: " << distances.at(0).distanceTo;
+                totalDistance += distances.at(0).distanceTo;
+                this->mostEffecientTrip.push_back(collegeClicked);
+                this->mostEffecientTrip.push_back(nextClosest);
+                ui->totalTripDistanceLabel->setText("Total Trip Distance: " + QString::number((totalDistance), 'f', 2) + " miles");
+                ui->nextClosestCollegeButton->setText("End Trip (Checkout)");
+                tripIterator = (mostEffecientTrip.end() - 1);
+                QListWidgetItem* newLabel = new QListWidgetItem(nextClosest.name);
+                newLabel->setBackgroundColor(Qt::red);
+                newLabel->setFlags(newLabel->flags() & ~Qt::ItemIsSelectable);
+                ui->cartListWidget->addItem(newLabel);
+                return;
+            }
+
+            // Add the starting college to the front of the mostEffecientTrip vector
+            this->mostEffecientTrip.push_back(collegeClicked);
+
+            totalDistance += distances.at(0).distanceTo;
+            totalDistancesIndexed.push_back(totalDistance);
+            recursivePathPlanner(nextClosest, this->mostEffecientTrip);
+
+            this->tripIterator = this->mostEffecientTrip.begin() + 1;
+
+            populateMenuItemDisplay(tripIterator->id);
+            ui->totalTripDistanceLabel->setText("Total Trip Distance: " + QString::number((totalDistancesIndexed.front())) + " miles");
+
+            // Display the name of the next closest college in UI button
+            ui->nextClosestCollegeButton->setText("Next Closest College:\n(" + (tripIterator + 1)->name + ")");
+            totalDistancesIndexed.pop_front();
+            collegeClicked = *tripIterator;
+            QListWidgetItem* newLabel = new QListWidgetItem(collegeClicked.name);
+            newLabel->setBackgroundColor(Qt::red);
+            newLabel->setFlags(newLabel->flags() & ~Qt::ItemIsSelectable);
+            ui->cartListWidget->addItem(newLabel);
+        }
+        // The user has already started their trip, and is traversing through the most effecient trip list vector (mostEffecientTrip)
+        else if (tripIterator != mostEffecientTrip.end()-1)
+        {
+            tripIterator++;
+            populateMenuItemDisplay(tripIterator->id);
+
+            // update total distance
+            ui->totalTripDistanceLabel->setText("Total Trip Distance: " + QString::number((totalDistancesIndexed.front())) + " miles");
+            totalDistancesIndexed.pop_front();
+            collegeClicked = *tripIterator;
+            QListWidgetItem* newLabel = new QListWidgetItem(collegeClicked.name);
+            newLabel->setBackgroundColor(Qt::red);
+            newLabel->setFlags(newLabel->flags() & ~Qt::ItemIsSelectable);
+            ui->cartListWidget->addItem(newLabel);
+
+            if (tripIterator == mostEffecientTrip.end() -1)
+            {
+                ui->nextClosestCollegeButton->setText("End Trip (Checkout)");
+            } else {
+                // Display the name of the next closest college in UI button
+                ui->nextClosestCollegeButton->setText("Next Closest College:\n(" + (tripIterator + 1)->name + ")");
+            }
+        }
+        else if (tripIterator == mostEffecientTrip.end()-1)
+        {
+            // Handle the end of the trip
+            shoppingCart.printCart();
+            this->close();
+        }
+    }
+}
+
+// Helper function for recursive path finder
+bool CollegeModel::vectorContains(QVector<College> colleges, College searchRest)
+{
+    for (int index=0; index < colleges.size(); index++)
+    {
+        if (colleges.at(index).id == searchRest.id)
+        {
+            return true;
+        }
+    }
+    // if it doesn't hit return true statement, the vector does not contain the given college.
+    return false;
+}
+
+
+
+
+void CollegeModel::on_cartListWidget_clicked(const QModelIndex &index)
+{
+    this->selectedRow = index.row();
+    qDebug() << selectedRow;
+    qDebug() << ui->cartListWidget->item(selectedRow)->data(Qt::UserRole).toInt();
+}
+
+void CollegeModel::on_removeCartItemButton_clicked()
+{
+    if (this->shoppingCart.size() > 0 && selectedRow != -2 && ui->cartListWidget->item(selectedRow))
+    {
+        if (ui->cartListWidget->item(selectedRow)->data(Qt::UserRole).toInt() == 0)
+        {
+            QMessageBox errorMsg;
+            errorMsg.setText("You can't remove the college name from your cart, silly! It's just a label. Try selecting an actual item from your cart.");
+            errorMsg.exec();
+            return;
+        }
+        qDebug() << "Before crash";
+        QMessageBox::StandardButton confirm;
+        confirm = QMessageBox::question(this, "Confirm Deletion", "Are you sure you want to remove the selected item from your cart?",
+                                        QMessageBox::Yes|QMessageBox::No);
+        if (confirm == QMessageBox::Yes) {
+            QListWidgetItem* itemToDelete = ui->cartListWidget->item(selectedRow);
+            MenuItem deletedItem = DatabaseManager::getInstance()->getMenuItemByID(itemToDelete->data(Qt::UserRole).toInt());
+            qDebug() << "Edit role: " << itemToDelete->data(128).toInt();
+            College collegePurchasedFrom = DatabaseManager::getInstance()->getCollegeByID(itemToDelete->data(128).toInt());
+            shoppingCart.deleteTransaction(Transaction(collegePurchasedFrom, deletedItem));
+            // Delete the actual item from memory
+            delete itemToDelete;
+
+            // Forces the user to re-select an item if they want to delete contiguous items from the cart
+            // Prevents accidental deletions
+            selectedRow=-2;
+        } else {
+            // Do nothing
+        }
+    }
+    else
+    {
+        QMessageBox errorMsg;
+        errorMsg.setText("Make sure to explicitly select an item from the cart each time you remove one!");
+        errorMsg.exec();
+    }
+}
+
+void CollegeModel::on_viewTotalsButton_clicked()
+{
+    if (shoppingCart.size() > 0)
+    {
+        TripWindow* totalsWindow = new TripWindow(shoppingCart, this);
+        totalsWindow->show();
+    }
+}
---
+++ collegemodel.h
@@ -0,0 +1,134 @@
+#ifndef CollegeModel_H
+#define CollegeModel_H
+
+#include <QWidget>
+#include <QPushButton>
+#include "cart.h"
+#include <QListWidgetItem>
+#include "tripwindow.h"
+namespace Ui {
+class CollegeModel;
+}
+
+//! The College User-Interface
+/*!
+  This class handles most functionality, along with
+  displays of the User-Interface and data being used.
+  The public methods will perform most capabilities
+  of the program. The private data members are used to
+  store and proccess data of the colleges. The class
+  allows the user to plan a trip to as many colleges
+  desired and purchase food as you would in a normal
+  college.
+*/
+
+class CollegeModel : public QWidget
+{
+    Q_OBJECT
+
+public:
+    //! Constructor
+    explicit CollegeModel(College collegeClicked, bool startingFromSaddleback=false, QWidget *parent = 0);
+    ~CollegeModel();
+
+    //! User-Interface function to populate the display with the given college's menu items.
+    /*!
+     * \brief populateMenuItemDisplay The college's ID is
+     * passed in to determine the current college and the
+     * menu is displayed in a gridLayout.
+     *
+     * \param collegeID An integer argument.
+     */
+    void populateMenuItemDisplay(int collegeID);
+
+    //! The recursive function to effectively plan the most efficient trip
+    /*!
+     * \brief recursivePathPlanner The entire efficient path
+     * is planned after start college is passed.
+     *
+     * The starting college is initially passed in and becomes
+     * the currentCollege along with the repeatedly updated mostEfficientList.
+     * The base case to exit the function is when the mostEfficientList's size is
+     * the user's desired amount of colleges. The recursive call is performed
+     * by passing in the next closest college toyour current location and set
+     * it as the currentCollege with an updated list.
+     *
+     * \param currentCollege A struct argument
+     * \param mostEffecientList A qvector argument
+     */
+    void recursivePathPlanner(College currentCollege, QVector<College>& mostEffecientList);
+
+    //! A search function to traverse through the list of colleges
+    /*!
+     * \brief vectorContains The function will traverse through colleges
+     * as it looks for the passed in searchRest and compare each one by ID.
+     *
+     * \param colleges A qvector argument
+     * \param searchRest A struct argument
+     * \return A boolean that is true if college is found
+     */
+    bool vectorContains(QVector<College> colleges, College searchRest);
+
+    //! A function that requires user-input of trip length
+    /*!
+     * \brief getTripLengthFromUser The user is prompted to enter
+     * the desired trip length in a QInputDialog box.
+     */
+    void getTripLengthFromUser();
+
+    //! To clear the widgets on a window
+    /*!
+     * \brief clearWidgets Will look through the layout for the
+     * pointed to poisiton and clear the widgets of that position.
+     * \param layout A qlayout argument
+     */
+    void clearWidgets(QLayout *layout);
+
+    //! A function for reassurance by user of a purchase
+    /*!
+     * \brief confirmPurchase The user is prompted if they
+     * confirm a purchase of a menu item in a QMessageBox.
+     *
+     * \param item A struct argument
+     */
+    void confirmPurchase(MenuItem item);
+
+    //! To add an item to the shopping cart
+    /*!
+     * \brief updateShoppingCart It will be passed the
+     * user's desired menu item and store it into their
+     * shopping cart.
+     *
+     * \param item A struct argument
+     */
+    void updateShoppingCart(MenuItem item);
+public slots:
+    //! Will activate the function to add the menu item to cart
+    void menuItemButtonPressed();
+private slots:
+
+    //! Will open the window to the next closest Colleges menu
+    void on_nextClosestCollegeButton_clicked();
+
+    //! Will open the window that displays the current shopping cart
+    void on_cartListWidget_clicked(const QModelIndex &index);
+
+    //! Will activate the function to delete a transaction
+    void on_removeCartItemButton_clicked();
+
+    //! Will open the total expenses window
+    void on_viewTotalsButton_clicked();
+
+private:
+    Ui::CollegeModel *ui;                   //!< The User-Interface for the class
+    Cart shoppingCart;                              //!< Vector of shopping cart that handles all colleges visited
+    College collegeClicked;                   //!< Struct of the currently clicked college by user
+    QVector<College> mostEffecientTrip;          //!< QVector of colleges that stores the most efficient trip
+    QVector<College>::iterator tripIterator;     //!< An iterator that is for traversals and processing of the trip
+    int numberOfCollegesToVisit;                 //!< The number of colleges the user has specified to visit
+    QVector<double> totalDistancesIndexed;          //!< QVector of double that will store the current indexed total distance
+    double totalDistance;                           //!< An accumulator of type double that will store the entire trip total distance
+    int selectedRow;                                //!< The selected row to access the database
+};
+
+#endif // CollegeModel_H
